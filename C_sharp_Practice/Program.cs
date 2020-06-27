7da26f1e18e3dcfd11fe636f6499c174e0e53236
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-06-27 14:51:31 -0500

    First Commit / setup

---
+++ C_sharp_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_sharp_Practice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
